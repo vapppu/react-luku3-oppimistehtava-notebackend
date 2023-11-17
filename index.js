4f8444a08639832406b07a4f0e97ddba1c81fd3e
Veera Hiltunen <veera.hiltunen@gmail.com>
2023-11-17 17:06:00 +0200

    Add custom middleware to log requests

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require('express')
 const app = express()
 
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:', request.path)
+  console.log('Body:', request.body)
+  console.log('---')
+  next()
+}
+
+app.use(express.json())
+app.use(requestLogger)
+
 let notes = [
     {
         id: 1,
@@ -25,6 +36,50 @@ app.get('/', (req, res) => {
 
 app.get('/api/notes', (req, res) => {
   res.json(notes)
+})
+
+app.get('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const note = notes.find(note => note.id === id)
+
+  if (note) {
+    response.json(note)
+  }
+  else {
+    response.status(404).end()
+  }
+})
+
+const generateId = () => {
+  const maxId = notes.length > 0
+  ? Math.max(...notes.map(n => n.id))
+  : 0
+  return maxId + 1
+}
+
+app.post('/api/notes', (request, response) => {
+
+  const body = request.body
+  if (!body.content) {
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    id: generateId(),
+  }
+
+  notes = notes.concat(note)
+  response.json(note)
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  notes = notes.filter(note => note.id !== id)
+  response.status(204).end()
 })
 
 const PORT = 3001
